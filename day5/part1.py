4c09ee0c752b6524d6a1e8b0228b460c42ea05d4
Jacky Hung <jacky5559tw@gmail.com>
2024-12-05 16:20:53 +0000

    5

---
+++ day5/part1.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+def solution(file):
+    with open(file, "r") as f:
+        rules, updates = f.read().split("\n\n")
+    
+    front_of = defaultdict(set)
+    for rule in rules.split("\n"):
+        a, b = rule.split("|")
+        front_of[b].add(a)
+    
+    res = 0
+    for pages in updates.split("\n"):
+        page = pages.split(",")
+        if all(page[i] in front_of[page[j]] for i in range(len(page)) for j in range(i+1, len(page))): 
+            res += int(page[len(page)//2])
+
+    return res
+
+if __name__ == "__main__":
+    assert solution("day5/test.txt") == 143
+    print( solution("day5/data.txt") )
---
+++ day5/part2.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+def solution(file):
+    with open(file, "r") as f:
+        rules, updates = f.read().split("\n\n")
+    
+    front_of = defaultdict(set)
+    for rule in rules.split("\n"):
+        a, b = rule.split("|")
+        front_of[b].add(a)
+    
+    res = 0
+    for pages in updates.split("\n"):
+        page = pages.split(",")
+        valid = True
+        for i in range(len(page)):
+            for j in range(i+1, len(page)):
+                if page[j] in front_of[page[i]]:
+                    page[i], page[j] = page[j], page[i]
+                    valid = False
+            if not valid and i == len(page)//2:
+                break
+        if not valid:
+            res += int(page[len(page)//2])
+
+    return res
+
+if __name__ == "__main__":
+    assert solution("day5/test.txt") == 123
+    print( solution("day5/data.txt") )
