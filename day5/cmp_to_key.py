08b66f72c979882befdbf6c9cdb591cecf87c04e
Jacky Hung <jacky5559tw@gmail.com>
2024-12-06 07:32:12 +0000

    5

---
+++ day5/cmp_to_key.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from functools import cmp_to_key
+
+def solution(file):
+    with open(file, "r") as f:
+        rules, updates = f.read().split("\n\n")
+    
+    front_of = defaultdict(set)
+    for rule in rules.split("\n"):
+        a, b = rule.split("|")
+        front_of[b].add(a)
+    
+    key = lambda a,b: -1 if a in front_of[b] else 1
+
+    res = 0
+    for update in updates.split("\n"):
+        pages = update.split(",")
+        lis = sorted(pages, key=cmp_to_key(key))
+        if lis != pages:
+            res += int(lis[len(lis)//2])
+
+    return res
+
+if __name__ == "__main__":
+    assert solution("day5/test.txt") == 123
+    print( solution("day5/data.txt") )
---
+++ day5/magic_method.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+class Key():
+    def __init__(self, page, front_of):
+        self.page = page
+        self.front_of = front_of
+    
+    def __lt__(self, others):
+        return self.page in self.front_of[others.page]
+    
+    def __int__(self):
+        return int(self.page)
+
+def solution(file):
+    with open(file, "r") as f:
+        rules, updates = f.read().split("\n\n")
+    
+    front_of = defaultdict(set)
+    for rule in rules.split("\n"):
+        a, b = rule.split("|")
+        front_of[b].add(a)
+    
+    res = 0
+    for update in updates.split("\n"):
+        pages = update.split(",")
+        lis = [Key(page, front_of) for page in pages]
+        sorted_lis = sorted(lis)
+        if sorted_lis != lis:
+            res += int(sorted_lis[len(sorted_lis)//2])
+
+    return res
+
+if __name__ == "__main__":
+    assert solution("day5/test.txt") == 123
+    print( solution("day5/data.txt") )
