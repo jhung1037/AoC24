a2a5307f5a16203b9c54751326e58f12472c4370
Jacky Hung <jacky5559tw@gmail.com>
2024-12-07 18:55:28 +0000

    7

---
+++ day7/part1.py
@@ -0,0 +1,27 @@
+def recur_check(ans, nums, i):
+    num = int(nums[i])
+    if i == 0:
+        return True if num == ans else False
+    if ans % num == 0 and recur_check(ans//num, nums, i-1):
+        return True
+    if ans - num >= 0 and recur_check(ans-num, nums, i-1):
+        return True
+    return False
+
+def solution(file):
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    res = 0
+    for line in lines:
+        str_ans, str_nums = line.split(":", 1)
+        nums = str_nums.split()
+        ans = int(str_ans)
+        if recur_check(ans, nums, len(nums)-1):
+            res += ans
+        
+    return res
+
+if __name__ == "__main__":
+    assert solution("day7/test.txt") == 3749
+    print( solution("day7/data.txt") )
