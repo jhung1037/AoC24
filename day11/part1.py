0fcb7039a5d96143e920ad187c45562d2c5f4c6b
Jacky Hung <jacky5559tw@gmail.com>
2024-12-11 22:26:16 +0000

    11

--- day11/part1.py
+++ day11/part1.py
@@ -2,7 +2,7 @@ def solution(file):
     with open(file, "r") as f:
         stones = list(map(int, f.readline().split()))
     
-    for times in range(25):
+    for _ in range(25):
         for i in range(len(stones)):
             if stones[i] == 0:
                 stones[i] = 1
@@ -11,9 +11,9 @@ def solution(file):
                 if length % 2:
                     stones[i] = stones[i] * 2024
                 else:
-                    digits = length // 2
-                    stones.append(stones[i] % (10 ** digits))
-                    stones[i] = stones[i] // (10 ** digits)
+                    q = 10 ** (length // 2)
+                    stones.append(stones[i] % q)
+                    stones[i] = stones[i] // q
 
     return len(stones)
 
--- day11/part2.py
+++ day11/part2.py
@@ -6,15 +6,15 @@ def dfs(num, rounds_left, dp):
     if num == 0:
         dp[(0, rounds_left)] = dfs(1, rounds_left - 1, dp)
         return dp[(0, rounds_left)]
+
+    length = len(str(num))
+    if length % 2:
+        dp[(num, rounds_left)] = dfs(num * 2024, rounds_left - 1, dp)
     else:
-        length = len(str(num))
-        if length % 2:
-            dp[(num, rounds_left)] = dfs(num * 2024, rounds_left - 1, dp)
-        else:
-            q = 10 ** (length // 2)
-            dp[(num, rounds_left)] = (dfs(num // q, rounds_left - 1, dp) + 
-                                      dfs(num % q, rounds_left - 1, dp))
-        return dp[(num, rounds_left)]
+        q = 10 ** (length // 2)
+        dp[(num, rounds_left)] = (dfs(num // q, rounds_left - 1, dp) + 
+                                  dfs(num % q, rounds_left - 1, dp))
+    return dp[(num, rounds_left)]
 
 def solution(file):
     with open(file, "r") as f:
