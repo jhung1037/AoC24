5d9c0ab237a33cb69098c0b301c50ca31d2b4fbd
Jacky Hung <jacky5559tw@gmail.com>
2024-12-18 15:04:42 +0000

    13

---
+++ day13/part2.py
@@ -0,0 +1,19 @@
+import re
+
+def solution(file):
+    with open(file, "r") as f:
+        combinations = f.read().split("\n\n")
+    
+    res = 0
+    for combination in combinations:
+        a1, a2, b1, b2, c1, c2 = list(map(int, re.findall("\d+", combination)))
+        c1, c2 = c1 + 10000000000000, c2 + 10000000000000
+        x = (b2*c1-b1*c2) / (a1*b2-b1*a2)
+        y = (a2*c1-a1*c2) / (b1*a2-a1*b2)
+        if int(x) == x and int(y) == y:
+            res += 3 * x + y
+
+    return int(res)
+
+if __name__ == "__main__":
+    print( solution("day13/data.txt") )
